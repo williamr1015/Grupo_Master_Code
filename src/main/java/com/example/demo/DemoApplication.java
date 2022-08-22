5fda57d2c54b8b7805ac6f4780c02b173ae54ad4
Chaccoman <diavete@gmail.com>
2022-08-21 23:36:19 -0500

    Se ingresa la segunta println

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.print("Hola mundo");
+		System.out.print("Se inserta esta linea de codigo");
 	}
 
 }
