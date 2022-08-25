488b0b4537cea7fa18bb573f754918651f82f425
William Rodriguez <williamrodriguez997@gmail.com>
2022-08-25 18:11:41 -0500

    Se agrega clase Transactions y se agrega correctamente a clase Main.

--- src/main/java/com/example/mastercode/Master_Code_App.java
+++ src/main/java/com/example/mastercode/Master_Code_App.java
@@ -8,9 +8,30 @@ public class Master_Code_App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Master_Code_App.class, args);
-		System.out.print("Hola mundo");
-		System.out.print("Se inserta esta linea de codigo");
-		System.out.print("Wilmar inserta ewsta line de código");
+		Transactions t = new Transactions(1, 100, "Ingreso", "Empresa 1", "2020-01-01", "2020-01-01");
+		System.out.println(t.getId());
+		System.out.println(t.getAmount());
+		System.out.println(t.getConcept());
+		System.out.println(t.getEnterprise());
+		System.out.println(t.getCreated_at());
+		System.out.println(t.getUpdated_at());
+
+		Transactions t2 = new Transactions(2, 200, "Ingreso", "Empresa 2", "2020-01-01", "2020-01-01");
+		System.out.println(t2.getId());
+		System.out.println(t2.getAmount());
+		System.out.println(t2.getConcept());
+		System.out.println(t2.getEnterprise());
+		System.out.println(t2.getCreated_at());
+		System.out.println(t2.getUpdated_at());
+
+		Transactions t3 = new Transactions(3, 50, "Retiro", "Empresa 3", "2020-01-01", "2020-01-01");
+		System.out.println(t3.getId());
+		System.out.println(t3.getAmount());
+		System.out.println(t3.getConcept());
+		System.out.println(t3.getEnterprise());
+		System.out.println(t3.getCreated_at());
+		System.out.println(t3.getUpdated_at());
+		
 	}
 
 }
